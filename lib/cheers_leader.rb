6bdc4a5366f63ec717122866bd6e48b25e739cce
Luan Ta <luan@tinypulse.com>
2018-05-05 21:56:51 +0700

    TODO app - Cheer Leader list with sorting & RSPEC

---
+++ lib/cheers_leader.rb
@@ -0,0 +1,21 @@
+class CheerLeaderList
+  def initialize(list)
+    @list = list
+  end
+attr_reader :list
+
+  def sort_by(sort_by = 'rank', asc = true)
+    return [] if list.to_a.empty?
+    case sort_by
+      when 'name'
+        asc ? list.sort_by {|x| x[1] } : list.sort {|x, y| y[1] <=> x[1] }
+      when 'sent'
+        asc ? list.sort_by {|x| x[2] } : list.sort_by {|x| -x[2] }  
+      when 'received'
+        asc ? list.sort_by {|x| x[3] } : list.sort_by {|x| -x[3] }  
+      else
+        asc ? list.sort_by {|x| x[0] } : list.sort_by {|x| -x[0] }  
+    end
+  end
+  
+end
